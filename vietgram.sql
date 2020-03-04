c496758bd1552adb559c5d234fc97ac6b5a76f21
Bayu Muhammad Iqbal <bayuiqballl13@gmail.com>
2020-03-05 02:50:10 +0700

    add sql

--- vietgram.sql
+++ vietgram.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2020 at 07:57 PM
+-- Generation Time: Mar 04, 2020 at 08:49 PM
 -- Server version: 10.1.40-MariaDB
 -- PHP Version: 7.3.5
 
@@ -39,23 +39,32 @@ CREATE TABLE `photo` (
 --
 
 INSERT INTO `photo` (`id`, `gambar`, `caption`) VALUES
-(10, 'Icons_CTF-690x500.png', 'BENDERA');
+(10, 'Icons_CTF-690x500.png', 'BENDERA'),
+(12, 'Laptop-ROG-ASUS.jpg', 'Laptopnya bagus banget\r\npengen beli dah, eh ga jadi saya jijik dengan ROG mending minjem saja hehehe ');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `profile`
+-- Table structure for table `profil`
 --
 
-CREATE TABLE `profile` (
+CREATE TABLE `profil` (
   `id` int(11) NOT NULL,
-  `username` varchar(225) NOT NULL,
+  `nama` varchar(225) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `website` varchar(255) NOT NULL,
-  `bio` varchar(255) NOT NULL,
+  `bio` varchar(1000) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `number_phone` varchar(255) NOT NULL,
-  `gender` varchar(136) NOT NULL
+  `number_phone` varchar(136) NOT NULL,
+  `gender` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profil`
+--
+
+INSERT INTO `profil` (`id`, `nama`, `username`, `website`, `bio`, `email`, `number_phone`, `gender`) VALUES
+(1, 'Bayu Muhammad Iqbal', 'bayuiqballl', 'https://github.com/bayuiqballl', 'The skills people and businesses n', 'bayuqballlll@gmail.com', '891234569', 'Male');
 
 -- --------------------------------------------------------
 
@@ -71,6 +80,13 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES
+(1, 'admin', '$2y$10$99kAnNr6Igcs5Os8rwAGb.L1vCXDmKgPvxs0SlNPn7m5KcOHA2g8e', 'bayuqballl@gmail.com');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -81,9 +97,9 @@ ALTER TABLE `photo`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `profile`
+-- Indexes for table `profil`
 --
-ALTER TABLE `profile`
+ALTER TABLE `profil`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -100,19 +116,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `photo`
 --
 ALTER TABLE `photo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `profile`
+-- AUTO_INCREMENT for table `profil`
 --
-ALTER TABLE `profile`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `profil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
